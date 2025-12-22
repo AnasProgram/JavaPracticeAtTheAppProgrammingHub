5ec2a3c697d034564edac7c1977964790d3387bf
Anas Abazid <anasabazid483@gmail.com>
2025-12-22 14:27:54 +0100

    MultiDimArrayExample

---
+++ Array/src/MultiDimArrayExample.java
@@ -0,0 +1,11 @@
+public class MultiDimArrayExample
+{
+    public static void main(String[] args)
+    {
+        String[][] names = {{"Mr.", "Ms."}, {"Smith", "Jones"}};
+//        Mr. Smith
+        System.out.println(names[0][0] + names[1][0]);
+//        Ms. Jones
+        System.out.println(names[0][1] + names[1][1]);
+    }
+}
